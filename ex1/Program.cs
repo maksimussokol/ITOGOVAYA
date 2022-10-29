71e9658d7f79838e73ba64c5eb5201628f947bd1
maksimussokol <maksimus@2074.ru>
2022-10-29 13:30:46 +0500

    dobavil vizualnoe predstavlenie methoda

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -2,13 +2,13 @@
 string[] array2 = new string[array1.Length];
 void newArray(string[] array1, string[] array2)
 {
-    int count = 0;
+    int k = 0;
     for (int i = 0; i < array1.Length; i++)
     {
         if (array1[i].Length <= 3)
         {
-            array2[count] = array1[i];
-            count++;
+            array2[k] = array1[i];
+            k++;
         }
     }
 }
